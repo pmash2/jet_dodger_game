0c8a65cef334072174aef552a83c65367169e27f
Phil <pmash2@users.noreply.github.com>
2021-10-22 23:01:04 -0400

    Keep player contained to screen

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
-player = Player()
+player = Player(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 running = True
 while running:
--- Player.py
+++ Player.py
@@ -7,18 +7,29 @@ from pygame.locals import(
 )
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self) -> None:
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT) -> None:
         super(Player, self).__init__()
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+        self.SCREEN_WIDTH = SCREEN_WIDTH
+        self.SCREEN_HEIGHT = SCREEN_HEIGHT
 
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
+            self.rect.move_ip(0, -2)
         if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
+            self.rect.move_ip(0, 2)
         if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
+            self.rect.move_ip(-2, 0)
         if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+            self.rect.move_ip(2, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > self.SCREEN_WIDTH:
+            self.rect.right = self.SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= self.SCREEN_HEIGHT:
+            self.rect.bottom = self.SCREEN_HEIGHT
