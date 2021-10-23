c3eebc3ec4843ebf518b7fbfef7838935e111fa0
Phil <pmash2@users.noreply.github.com>
2021-10-22 22:17:42 -0400

    Initialize game

---
+++ Main.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+# https://realpython.com/pygame-a-primer/#images-and-rects
+
+pygame.init()
+
+screen = pygame.display.set_mode([500, 500])
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(pygame.Color('white'))
+
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    pygame.display.flip()
+
+pygame.quit()
