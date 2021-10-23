982a0e94d8378edac82bb0cc223d530c134132b1
Phil <pmash2@users.noreply.github.com>
2021-10-22 22:26:33 -0400

    Import keys, change window size, and add Escape

--- Main.py
+++ Main.py
@@ -1,15 +1,31 @@
 import pygame
 
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
 
 # https://realpython.com/pygame-a-primer/#images-and-rects
 
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 running = True
 while running:
     for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
         if event.type == pygame.QUIT:
             running = False
 
